8c35a9efb0a752d6e479630fbe1ae05b468e1cdb
Igor Mokhov <igormokhovid@gmail.com>
2025-03-13 17:35:30 +0700

    feat: added able to open Modal in the Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,15 @@
+import { useAppDispatch } from '../../redux/hooks';
+import { openModal } from '../../redux/slices/modal/modalSlice';
 import { Button } from '../../UI/Button/Button';
 import styles from './Header.module.scss';
 
 export const Header = () => {
+  const dispatch = useAppDispatch();
+
   return (
     <header className={styles.header}>
-      <h1 className={styles.header_title}>Crypto Portfolio</h1>
-      <Button>добавить</Button>
+      <h1 className={styles.header_title}>CRYPTO PORTFOLIO</h1>
+      <Button onClick={() => dispatch(openModal())}>добавить</Button>
     </header>
   );
 };
