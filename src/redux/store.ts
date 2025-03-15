65cce7193a324b4fb8513aeae51c1fab5ea9e71c
Igor Mokhov <igormokhovid@gmail.com>
2025-03-15 22:12:47 +0700

    feat: added new reducers to store

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { modalReducer } from './slices/modal/modalSlice';
+import { portfolioApi } from './api/portfolio/portfolioApi';
+import { portfolioReducer } from './slices/portfolio/portfolioSlice';
 
 export const store = configureStore({
   reducer: {
     modal: modalReducer,
+    portfolio: portfolioReducer,
+    [portfolioApi.reducerPath]: portfolioApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(portfolioApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
