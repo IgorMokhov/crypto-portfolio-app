a1159db16b8cac56ffbc663c8feae8ceb01bfa59
Igor Mokhov <igormokhovid@gmail.com>
2025-03-15 22:15:40 +0700

    feat: added loading of available currencies with subsequent saving in the redux slice

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -1,15 +1,38 @@
+import { useEffect } from 'react';
+import { useGetAvailableCurrenciesQuery } from '../../redux/api/portfolio/portfolioApi';
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 import { closeModal, selectModal } from '../../redux/slices/modal/modalSlice';
 import { Modal } from '../../UI/Modal/Modal';
+import { CurrencySelector } from '../CurrencySelector/CurrencySelector';
+import { setAvailableCurrencies } from '../../redux/slices/portfolio/portfolioSlice';
+import { transformAvailableCurrencies } from '../../utils/currencyUtils';
 import styles from './Portfolio.module.scss';
 
 export const Portfolio = () => {
   const isOpen = useAppSelector(selectModal);
   const dispatch = useAppDispatch();
 
+  const { data, isSuccess, isError, error } = useGetAvailableCurrenciesQuery();
+
+  useEffect(() => {
+    if (isSuccess) {
+      dispatch(setAvailableCurrencies(transformAvailableCurrencies(data)));
+    }
+  }, [isSuccess]);
+
+  useEffect(() => {
+    if (isError) {
+      console.log(error);
+    }
+  }, [isError]);
+
   return (
-    <main className={styles.portfolio}>
-      {isOpen && <Modal onClose={() => dispatch(closeModal())} />}
-    </main>
+    <section className={styles.portfolio}>
+      {isOpen && (
+        <Modal onClose={() => dispatch(closeModal())}>
+          <CurrencySelector />
+        </Modal>
+      )}
+    </section>
   );
 };
