6a0cc775728b219ba8c6de388332a98193ebc314
Igor Mokhov <igormokhovid@gmail.com>
2025-03-15 17:28:34 +0700

    feat: added support for custom content in Modal

--- src/UI/Modal/Modal.tsx
+++ src/UI/Modal/Modal.tsx
@@ -1,23 +1,19 @@
+import { ReactNode } from 'react';
 import styles from './Modal.module.scss';
 
 interface IModalProps {
+  children: ReactNode;
   onClose: () => void;
 }
 
-export const Modal = ({ onClose }: IModalProps) => {
+export const Modal = ({ children, onClose }: IModalProps) => {
   const handleBackDropClick = (e: React.MouseEvent<HTMLDivElement>) => {
     if (e.currentTarget === e.target) onClose();
   };
 
   return (
     <div className={styles.backdrop} onClick={handleBackDropClick}>
-      <div className={styles.modal}>
-        <input
-          className={styles.modal_input}
-          type="text"
-          placeholder="Поиск валюты"
-        />
-      </div>
+      <div className={styles.modal}>{children}</div>
     </div>
   );
 };
