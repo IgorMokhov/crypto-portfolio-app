02d8dbc63261b1451c5292e84b593f8b196d3719
Igor Mokhov <igormokhovid@gmail.com>
2025-03-15 22:16:49 +0700

    feat: added display of a list of currencies when opening a modal window

--- src/components/AvailableCurrencyList/AvailableCurrencyList.tsx
+++ src/components/AvailableCurrencyList/AvailableCurrencyList.tsx
@@ -1,10 +1,21 @@
+import { useAppSelector } from '../../redux/hooks';
+import { selectCurrencies } from '../../redux/slices/portfolio/portfolioSlice';
 import { AvailableCurrencyItem } from '../AvailableCurrencyItem/AvailableCurrencyItem';
 import styles from './AvailableCurrencyList.module.scss';
 
 export const AvailableCurrencyList = () => {
+  const currencies = useAppSelector(selectCurrencies);
+
   return (
-    <ul className={styles.tokenList}>
-      <AvailableCurrencyItem name={'BTC'} price={851.0022} change={3.3} />
+    <ul className={styles.currencyList}>
+      {currencies?.map(({ name, price, priceChange }) => (
+        <AvailableCurrencyItem
+          name={name}
+          price={price}
+          priceChange={priceChange}
+          key={name}
+        />
+      ))}
     </ul>
   );
 };
