bf24b4899331cc3109535538025dabdccc049e8e
Igor Mokhov <igormokhovid@gmail.com>
2025-03-17 20:02:28 +0700

    feat: added SelectedCurrencyItem with styles

---
+++ src/components/SelectedCurrencyItem/SelectedCurrencyItem.tsx
@@ -0,0 +1,25 @@
+import { ICurrency } from '../../types/currencies';
+import styles from './SelectedCurrencyItem.module.scss';
+
+interface ISelectedCurrencyItemProps extends ICurrency {}
+
+export const SelectedCurrencyItem = ({
+  name,
+  quantity,
+  price,
+  priceChange,
+}: ISelectedCurrencyItemProps) => {
+  return (
+    <li className={styles.currencyItem}>
+      <span>{name}</span>
+      <span>{quantity}</span>
+      <span>${price}</span>
+      <span>${price * (quantity || 1)}</span>
+      <span className={`${styles.currencyItem_change} ${priceChange < 0 ? styles.negative : ''}`}>
+        {priceChange > 0 ? '+' : ''}
+        {priceChange}%
+      </span>
+      <span>100%</span>
+    </li>
+  );
+};
