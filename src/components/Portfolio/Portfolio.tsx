15cc90ec72944ebd9da6f639ae9a7d8c11051667
Igor Mokhov <igormokhovid@gmail.com>
2025-03-17 20:05:03 +0700

    feat: added loading of data using a hook useCurrencyStream with subsequent rendering

--- src/components/Portfolio/Portfolio.tsx
+++ src/components/Portfolio/Portfolio.tsx
@@ -6,11 +6,15 @@ import { Modal } from '../../UI/Modal/Modal';
 import { CurrencySelector } from '../CurrencySelector/CurrencySelector';
 import { setAvailableCurrencies } from '../../redux/slices/portfolio/portfolioSlice';
 import { transformAvailableCurrencies } from '../../utils/currencyUtils';
+import { useCurrencyStream } from '../../hooks/useCurrencyStream';
 import styles from './Portfolio.module.scss';
+import { SelectedCurrencyList } from '../SelectedCurrencyList/SelectedCurrencyList';
 
 export const Portfolio = () => {
   const isOpen = useAppSelector(selectModal);
   const dispatch = useAppDispatch();
+
+  const currencies = useCurrencyStream();
 
   const { data, isSuccess, isError, error } = useGetAvailableCurrenciesQuery();
 
@@ -28,6 +32,8 @@ export const Portfolio = () => {
 
   return (
     <section className={styles.portfolio}>
+      <SelectedCurrencyList currencies={currencies} />
+
       {isOpen && (
         <Modal onClose={() => dispatch(closeModal())}>
           <CurrencySelector />
